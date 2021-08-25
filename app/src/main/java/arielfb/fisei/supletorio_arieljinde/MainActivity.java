be3d26ae40f4430bccff3430aec58ba4d9559921
Ariel <ajinde3033@uta.edu.ec>
2021-08-25 18:11:11 -0500

    First

---
+++ app/src/main/java/arielfb/fisei/supletorio_arieljinde/MainActivity.java
@@ -0,0 +1,13 @@
+package arielfb.fisei.supletorio_arieljinde;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
