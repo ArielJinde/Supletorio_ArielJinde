fdee12b390f249d5a8c5dc0896b55fff17bf8342
Ariel <ajinde3033@uta.edu.ec>
2021-08-25 21:06:36 -0500

    4

---
+++ app/src/main/java/arielfb/fisei/supletorio_arieljinde/CrearTarea_AJ.java
@@ -0,0 +1,14 @@
+package arielfb.fisei.supletorio_arieljinde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearTarea_AJ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_tarea_aj);
+    }
+}
--- app/src/main/java/arielfb/fisei/supletorio_arieljinde/HomeMainActivity_AJ.java
+++ app/src/main/java/arielfb/fisei/supletorio_arieljinde/HomeMainActivity_AJ.java
@@ -1,14 +1,25 @@
 package arielfb.fisei.supletorio_arieljinde;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeMainActivity_AJ extends AppCompatActivity {
+    private DatabaseReference databaseReference;
+    private DatabaseReference mDatabase;
 
     TabLayout tabLayout;
     ViewPager viewPager;
@@ -20,7 +31,7 @@ public class HomeMainActivity_AJ extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_main_aj);
-
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         tabLayout = findViewById(R.id.tablayout_aj);
         viewPager =  findViewById(R.id.viewPager_aj);
         personal  = findViewById(R.id.personal_aj);
@@ -55,4 +66,33 @@ public class HomeMainActivity_AJ extends AppCompatActivity {
         viewPager.addOnPageChangeListener( new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
     }
+
+    @Override
+    public  boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_aj,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.agregar_tarea){
+            Toast.makeText(HomeMainActivity_AJ.this, "Opcion 1s", Toast.LENGTH_SHORT).show();
+        }
+        if (id == R.id.borrar_tareas){
+            Toast.makeText(HomeMainActivity_AJ.this, "Opcion 1s", Toast.LENGTH_SHORT).show();
+
+        }
+        if (id == R.id.cerrar_sesion){
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(HomeMainActivity_AJ.this, MainActivity_AJ.class));
+
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+
+
+
 }
